64c5c7f2d9d9033b740a8b900b24cb5c345db29d
devson <sonyc5720@gmail.com>
2021-01-01 23:31:16 +0900

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,78 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.4.1"
+	id("io.spring.dependency-management") version "1.0.10.RELEASE"
+	id("org.asciidoctor.convert") version "1.5.8"
+	kotlin("jvm") version "1.4.21"
+	kotlin("plugin.spring") version "1.4.21"
+}
+
+group = "io.github.ivvve"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+extra["snippetsDir"] = file("build/generated-snippets")
+extra["springCloudVersion"] = "2020.0.0"
+extra["testcontainersVersion"] = "1.15.0"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.liquibase:liquibase-core")
+	implementation("org.springframework.cloud:spring-cloud-starter-vault-config")
+	implementation("org.springframework.session:spring-session-core")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.h2database:h2")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+	testImplementation("org.testcontainers:junit-jupiter")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks {
+    withType<Test> {
+		useJUnitPlatform()
+	}
+
+    val snippetsDir = extra["snippetsDir"]!!
+	val testTask = withType<Test> {
+		outputs.dir(snippetsDir)
+	}
+
+    withType<org.asciidoctor.gradle.AsciidoctorTask> {
+		dependsOn(testTask)
+		inputs.dir(snippetsDir)
+		dependsOn(test)
+	}
+}
---
+++ src/test/kotlin/io/github/ivvve/bookmanager/BookManagerApplicationTests.kt
@@ -0,0 +1,14 @@
+package io.github.ivvve.bookmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+class BookManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
