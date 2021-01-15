c7ba0bbcd64e27b6a0729e472dae71b5a91af054
devson <sonyc5720@gmail.com>
2021-01-15 22:42:30 +0900

    feat: implement VerificationCodeStore

---
+++ src/main/kotlin/io/github/ivvve/bookmanager/domain/account/infrastructure/verification/MemoryEmailVerificationCodeStore.kt
@@ -0,0 +1,20 @@
+package io.github.ivvve.bookmanager.domain.account.infrastructure.verification
+
+import io.github.ivvve.bookmanager.domain.account.domain.verification.entities.VerificationCode
+import io.github.ivvve.bookmanager.domain.account.domain.verification.VerificationCodeStore
+import org.springframework.stereotype.Repository
+import java.util.concurrent.ConcurrentHashMap
+
+@Repository
+class MemoryEmailVerificationCodeStore : VerificationCodeStore {
+    private val store = ConcurrentHashMap<String, VerificationCode>()
+
+    override fun save(code: VerificationCode): VerificationCode {
+        this.store[code.email] = code
+        return code
+    }
+
+    override fun findByEmail(email: String): VerificationCode? {
+        return this.store[email]
+    }
+}
