e46946fe23c8ca8adc90131dc606001dc824cf7b
devson <sonyc5720@gmail.com>
2021-01-15 21:39:16 +0900

    feat: add VerificationCodeValidator to validate verification code

---
+++ src/main/kotlin/io/github/ivvve/bookmanager/domain/account/domain/verification/exceptions/VerificationCodeNotFoundException.kt
@@ -0,0 +1,6 @@
+package io.github.ivvve.bookmanager.domain.account.domain.verification.exceptions
+
+import io.github.ivvve.bookmanager.common.exceptions.NotFoundException
+
+class VerificationCodeNotFoundException : NotFoundException() {
+}
