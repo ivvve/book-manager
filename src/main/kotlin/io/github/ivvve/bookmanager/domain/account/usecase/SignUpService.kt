e6084dc09f2a492e77f3cc80458cc253b86709a5
devson <sonyc5720@gmail.com>
2021-01-16 00:07:23 +0900

    feat: add Password Hashing feature

--- src/main/kotlin/io/github/ivvve/bookmanager/domain/account/usecase/SignUpService.kt
+++ src/main/kotlin/io/github/ivvve/bookmanager/domain/account/usecase/SignUpService.kt
@@ -3,6 +3,7 @@ package io.github.ivvve.bookmanager.domain.account.usecase
 import io.github.ivvve.bookmanager.domain.account.domain.account.AccountRepository
 import io.github.ivvve.bookmanager.domain.account.domain.account.DuplicatedAccountChecker
 import io.github.ivvve.bookmanager.domain.account.domain.account.EmailValidator
+import io.github.ivvve.bookmanager.domain.account.domain.account.PasswordEncryptor
 import io.github.ivvve.bookmanager.domain.account.domain.account.entities.Account
 import io.github.ivvve.bookmanager.domain.account.domain.verification.VerificationCodeGenerator
 import io.github.ivvve.bookmanager.domain.account.domain.verification.VerificationCodeSender
@@ -19,6 +20,8 @@ class SignUpService(
     private val verificationCodeStore: VerificationCodeStore,
     private val verificationCodeValidator: VerificationCodeValidator,
     private val verificationCodeSender: VerificationCodeSender,
+
+    private val passwordEncryptor: PasswordEncryptor,
     private val duplicatedAccountChecker: DuplicatedAccountChecker,
     private val accountRepository: AccountRepository
 ) {
@@ -35,8 +38,9 @@ class SignUpService(
     fun signUp(email: String, password: String, verificationCode: String): Account {
         this.verificationCodeValidator.validate(VerificationCode(email, verificationCode))
 
-        // TODO encrypt password
-        val account = Account(UUID.randomUUID(), email, password)
+        val encryptedPassword = this.passwordEncryptor.encrypt(password)
+        val account = Account(UUID.randomUUID(), email, encryptedPassword)
+
         this.duplicatedAccountChecker.check(account)
         return this.accountRepository.save(account)
     }
