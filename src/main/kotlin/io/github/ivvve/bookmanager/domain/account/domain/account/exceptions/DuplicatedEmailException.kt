7c69d18526a1b7b6c6148a97166cb1782a92922d
devson <sonyc5720@gmail.com>
2021-01-15 23:58:38 +0900

    feat: add DuplicatedEmailException

---
+++ src/main/kotlin/io/github/ivvve/bookmanager/domain/account/domain/account/exceptions/DuplicatedEmailException.kt
@@ -0,0 +1,5 @@
+package io.github.ivvve.bookmanager.domain.account.domain.account.exceptions
+
+import io.github.ivvve.bookmanager.common.exceptions.DuplicationException
+
+class DuplicatedEmailException : DuplicationException()
