eb2a5e457ef00078c8d8649de110b90181fe2085
devson <sonyc5720@gmail.com>
2021-01-15 23:22:19 +0900

    feat: add tearDown to clear data

--- src/test/kotlin/io/github/ivvve/bookmanager/acceptance/AcceptanceTest.kt
+++ src/test/kotlin/io/github/ivvve/bookmanager/acceptance/AcceptanceTest.kt
@@ -1,9 +1,13 @@
 package io.github.ivvve.bookmanager.acceptance
 
 import io.github.ivvve.bookmanager.BookManagerApplication
+import io.github.ivvve.bookmanager.domain.account.domain.account.AccountRepository
+import io.github.ivvve.bookmanager.domain.account.domain.verification.VerificationCodeStore
 import io.restassured.RestAssured
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 
@@ -13,8 +17,19 @@ class AcceptanceTest {
     @LocalServerPort
     var port = 0
 
+    @Autowired
+    private lateinit var verificationCodeStore: VerificationCodeStore
+    @Autowired
+    private lateinit var accountRepository: AccountRepository
+
     @BeforeAll
     fun setUp() {
         RestAssured.port = this.port
+    }
+
+    @AfterEach
+    fun tearDown() {
+        this.verificationCodeStore.clear()
+        this.accountRepository.deleteAll()
     }
 }
