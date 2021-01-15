5a4a4a8579eb77000b5ce00cce8f1ac5d17d647b
devson <sonyc5720@gmail.com>
2021-01-15 22:30:45 +0900

    test: add VerificationCodeHelper component

--- src/test/kotlin/io/github/ivvve/bookmanager/acceptance/account/SignUpAcceptanceTest.kt
+++ src/test/kotlin/io/github/ivvve/bookmanager/acceptance/account/SignUpAcceptanceTest.kt
@@ -1,13 +1,15 @@
 package io.github.ivvve.bookmanager.acceptance.account
 
 import io.github.ivvve.bookmanager.acceptance.AcceptanceTest
-import io.github.ivvve.bookmanager.acceptance.account.step.`이메일 인증번호 전송 요청에 성공한다`
-import io.github.ivvve.bookmanager.acceptance.account.step.`이메일 인증번호 전송 요청을 보낸다`
-import io.github.ivvve.bookmanager.acceptance.account.step.`회원가입 요청을 보낸다`
-import io.github.ivvve.bookmanager.acceptance.account.step.`회원가입에 성공한다`
+import io.github.ivvve.bookmanager.acceptance.account.helper.VerificationCodeHelper
+import io.github.ivvve.bookmanager.acceptance.account.step.*
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 
 class SignUpAcceptanceTest : AcceptanceTest() {
+    @Autowired
+    private lateinit var verificationCodeHelper: VerificationCodeHelper
+
     @Test
     fun `회원가입을 한다`() {
         // given
@@ -20,9 +22,8 @@ class SignUpAcceptanceTest : AcceptanceTest() {
         `이메일 인증번호 전송 요청에 성공한다`(emailVerificationResponse)
 
         // when
-        // TODO get verification code
-        val verificationCode = "000000"
-        val signUpResponse = `회원가입 요청을 보낸다`(email, password, verificationCode)
+        val verificationCode = this.verificationCodeHelper.getVerificationCode(email)
+        val signUpResponse = `회원가입 요청을 보낸다`(email, password, verificationCode.code)
         // then
         `회원가입에 성공한다`(signUpResponse)
     }
