ed735b0af69add514cd91ff8122e0caa88b357e8
devson <sonyc5720@gmail.com>
2021-01-15 22:41:55 +0900

    feat: add EmailValidator feature

---
+++ src/main/kotlin/io/github/ivvve/bookmanager/domain/account/domain/account/exceptions/NotSupportedEmailException.kt
@@ -0,0 +1,6 @@
+package io.github.ivvve.bookmanager.domain.account.domain.account.exceptions
+
+import io.github.ivvve.bookmanager.common.exceptions.BadRequestException
+
+class NotSupportedEmailException : BadRequestException() {
+}
